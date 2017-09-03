57796d8381faafdd5a60930ee1d3b72d27e87f61
didierNar <yovanni.n.r@gmail.com>
2017-09-03 18:03:53 -0500

    Audio

--- app/src/main/java/archivosmultimedia/eam/archivosmultimedia/GrabarAudio.java
+++ app/src/main/java/archivosmultimedia/eam/archivosmultimedia/GrabarAudio.java
@@ -18,12 +18,14 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import archivosmultimedia.eam.archivosmultimedia.modelo.Reunion;
 
-public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnCompletionListener{
+public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnCompletionListener {
 
     MediaRecorder recoder;
 
@@ -37,107 +39,112 @@ public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnComp
 
     ArrayList<String> audios;
 
-    Button btnGrabarAudio,btnDetener;
+    Button btnGrabarAudio, btnDetener;
+
+    ArrayList<File> archivos;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grabar_audio);
-        btnGrabarAudio = (Button)findViewById(R.id.btnGrabarAudio);
-        btnDetener = (Button)findViewById(R.id.btnDetener);
+        btnGrabarAudio = (Button) findViewById(R.id.btnGrabarAudio);
+        btnDetener = (Button) findViewById(R.id.btnDetener);
         listaAudios = (ListView) findViewById(R.id.listaAudios);
         audios = new ArrayList<>();
+        archivos = new ArrayList<>();
+
         cargarNumero();
         listarAudios();
+        listarArchivos();
+
+        btnDetener.setEnabled(false);
 
 
         listaAudios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String audioSelec = (String) listaAudios.getItemAtPosition(position);
-
+                reproducir(position);
             }
         });
 
     }
 
+    public void reproducir(int pos) {
+        try {
+
+            player = new MediaPlayer();
+            player.setOnCompletionListener(this);
+            archivo = archivos.get(pos);
+
+            player.setDataSource(archivo.getAbsolutePath());
+            player.prepare();
+
+            player.start(); // Se reproduce el archivo
+            btnGrabarAudio.setEnabled(false);
+            btnDetener.setEnabled(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public void grabarAudio(View v) {
 
+        try {
 
-        if (cantidadAudios == 0) {
+            recoder = new MediaRecorder();
 
-            try {
+            //definimos el microfono como fuente del audio
+            recoder.setAudioSource(MediaRecorder.AudioSource.MIC);
 
-                recoder = new MediaRecorder();
+            //definimos el formato de salida en 3.GP
+            recoder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 
-                //definimos el microfono como fuente del audio
-                recoder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            //definimos el codec para el 3.GP
+            recoder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-                //definimos el formato de salida en 3.GP
-                recoder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            //obtenemos la ruta absoluta de la interna y se crea un archivo temporal
+            File ruta = new File(Environment.getExternalStorageDirectory().getPath());
 
-                //definimos el codec para el 3.GP
-                recoder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-                //obtenemos la ruta absoluta de la onterna t se crea un archivo temporal
-                File ruta = new File(Environment.getExternalStorageDirectory().getPath() + Reunion.getActual().getNombre() + "1");
-
-                //creamos el archivo donde se almacena el audio
-                archivo = File.createTempFile("temporal", ".3GP", ruta);
-                //definimos que el archivo  creado es donde se almacena el audio
-                recoder.setOutputFile(archivo.getAbsolutePath());
-
-                //preparamos el archivo para iniciar la grabacion
-                recoder.prepare();
-
-                //iniciamos la grabacion
-                recoder.start();
-
-                btnGrabarAudio.setText("Grabando");
-                btnGrabarAudio.setEnabled(false);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-
-            try {
-                recoder = new MediaRecorder();
-
-                //definimos el microfono como fuente del audio
-                recoder.setAudioSource(MediaRecorder.AudioSource.MIC);
-
-                //definimos el formato de salida en 3.GP
-                recoder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-
-                //definimos el codec para el 3.GP
-                recoder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-                //obtenemos la ruta absoluta de la onterna t se crea un archivo temporal
-                File ruta = new File(Environment.getExternalStorageDirectory().getPath() +
-                        Reunion.getActual().getNombre() + cantidadAudios + 1);
+            if (cantidadAudios == 0) {
 
                 //creamos el archivo donde se almacena el audio
-                archivo = File.createTempFile("temporal", ".3GP", ruta);
-                //definimos que el archivo  creado es donde se almacena el audio
-                recoder.setOutputFile(archivo.getAbsolutePath());
+                archivo = File.createTempFile(Reunion.getActual().getNombre() + "1",
+                        ".3GP", ruta);
 
-                //preparamos el archivo para iniciar la grabacion
-                recoder.prepare();
+            } else {
 
-                //iniciamos la grabacion
-                recoder.start();
+                //creamos el archivo donde se almacena el audio
+                archivo = File.createTempFile(Reunion.getActual().getNombre()
+                        + cantidadAudios + 1, ".3GP", ruta);
 
-                btnGrabarAudio.setText("Grabando");
-                btnGrabarAudio.setEnabled(false);
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+
+            archivos.add(archivo);
+
+            //definimos que el archivo  creado es donde se almacena el audio
+            recoder.setOutputFile(archivo.getAbsolutePath());
+
+            //preparamos el archivo para iniciar la grabacion
+            recoder.prepare();
+
+            //iniciamos la grabacion
+            recoder.start();
+
+            btnGrabarAudio.setEnabled(false);
+            btnDetener.setEnabled(true);
+
+            cantidadAudios++;
+            guardar();
+            guardarArchivo();
+            listarAudios();
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        cantidadAudios++;
-        guardar();
-        listarAudios();
+
     }
 
 
@@ -162,7 +169,7 @@ public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnComp
             String[] a = fileList();
             if (existeArchivo(a, Reunion.getNomArchivo())) {
                 InputStreamReader reader = new InputStreamReader(
-                        openFileInput("videos" + Reunion.getActual().getNombre() + ".txt"));
+                        openFileInput("audios" + Reunion.getActual().getNombre() + ".txt"));
                 BufferedReader br = new BufferedReader(reader);
                 String linea = br.readLine();
                 if (linea == null) {
@@ -178,21 +185,21 @@ public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnComp
         }
     }
 
-    public void listarAudios(){
+    public void listarAudios() {
 
-           audios.clear();
-            if (cantidadAudios == 0){
-                Toast.makeText(this, "No hay audios de esta reunión",
-                        Toast.LENGTH_SHORT).show();
-            } else {
-                for (int i = 1; i <= cantidadAudios; i++) {
-                    String nombre = Reunion.getActual().getNombre() + i + ".3GP";
-                    audios.add(nombre);
-                }
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, audios);
-                listaAudios.setAdapter(adapter);
+        audios.clear();
+        if (cantidadAudios == 0) {
+            Toast.makeText(this, "No hay audios de esta reunión",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            for (int i = 1; i <= cantidadAudios; i++) {
+                String nombre = Reunion.getActual().getNombre() + i + ".3GP";
+                audios.add(nombre);
             }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, audios);
+            listaAudios.setAdapter(adapter);
+        }
 
     }
 
@@ -241,14 +248,42 @@ public class GrabarAudio extends AppCompatActivity implements MediaPlayer.OnComp
         btnGrabarAudio.setEnabled(true);
         //bloqueamos el boton de detener
         btnDetener.setEnabled(false);
-
-        btnGrabarAudio.setText("Listo para reproducir");
     }
 
     @Override
     public void onCompletion(MediaPlayer mediaPlayer) {
         btnGrabarAudio.setEnabled(true);
         //bloqueamos el boton de detener
-        btnDetener.setEnabled(true);
+        btnDetener.setEnabled(false);
+    }
+
+    public void listarArchivos() {
+        try {
+            String[] a = fileList();
+            if (existeArchivo(a, "archivo"+Reunion.getActual().getNombre()+".txt")) {
+                ObjectInputStream reader = new ObjectInputStream(openFileInput
+                        ("archivo"+Reunion.getActual().getNombre()+".txt"));
+                archivos = (ArrayList<File>) reader.readObject();
+                reader.close();
+            }
+        } catch (IOException e) {
+
+        } catch (ClassNotFoundException c) {
+
+        }
+    }
+
+    public void guardarArchivo() {
+        try {
+            ObjectOutputStream writer = new ObjectOutputStream
+                    (openFileOutput("archivo"+Reunion.getActual().getNombre()+".txt",
+                            Activity.MODE_PRIVATE));
+            writer.writeObject(archivos);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            Toast.makeText(this, "Error al crear el archivo", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
